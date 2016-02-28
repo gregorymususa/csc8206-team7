6b36241c8d181c11e067c0c34170132747edb5af
wergre <X.Yuan8@newcastle.ac.uk>
2016-02-28 00:26:37 +0000

    Block may have errors pls help

--- src/Block.java
+++ src/Block.java
@@ -1,9 +1,41 @@
 
 /**
- * Small change
+ * Implements the Block, as part of the Railway Interlocking system
  * @author Team 7
  *
  */
 public class Block {
+
+	private String name;
+	
+	public Block(String name) {
+		this.name = name;
+	}
+
+	public Block(String upNeigh, String downNeigh) {
+		if (
+		
+	}
+/**
+ * tells if it is clear
+ * @return isClear
+ */
+	public String getIsClear(){
+		String c = isclear;
+		return c;	
+	}
+/**	tells true or false if any trains are on the tracks	
+ * 
+ * @param c
+ * @return ture or false
+ */
+	public boolean setIsClear(String c){
+		if(!(c==null))
+			return false;
+		
+		else
+		return true;
+	}
+
 
 }
